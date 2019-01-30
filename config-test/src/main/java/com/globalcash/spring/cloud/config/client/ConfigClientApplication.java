6242c35c2adbc43a61d5827de0a2a1e84e45bbbf
unknown <632422571@qq.com>
2019-01-30 19:43:14 +0800

    test config

---
+++ config-test/src/main/java/com/globalcash/spring/cloud/config/client/ConfigClientApplication.java
@@ -0,0 +1,16 @@
+package com.globalcash.spring.cloud.config.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author gh
+ * @date 2019/1/30 18:54
+ */
+
+@SpringBootApplication
+public class ConfigClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class,args);
+    }
+}
